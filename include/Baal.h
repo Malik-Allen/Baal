ee9fa68e243558eb83bfd840f911489109d63785
Malik Allen <43439604+Malik-Allen@users.noreply.github.com>
2024-08-01 00:03:47 -0400

    Updated include header

--- include/Baal.h
+++ include/Baal.h
@@ -4,6 +4,23 @@
 #define BAAL_H
 
 #include "../src/core/vulkan/Renderer.h"
+#include "../src/core/vulkan/debugging/Error.h"
+#include "../src/core/vulkan/initialization/Instance.h"
+#include "../src/core/vulkan/devices/PhysicalDevice.h"
+#include "../src/core/vulkan/devices/LogicalDevice.h"
+#include "../src/core/vulkan/presentation/Surface.h"
+#include "../src/core/vulkan/resource/Buffer.h"
+#include "../src/core/vulkan/commands/CommandBuffer.h"
+#include "../src/core/vulkan/presentation/SwapChain.h"
+#include "../src/core/vulkan/pipeline/RenderPass.h"
+#include "../src/core/vulkan/pipeline/Framebuffer.h"
+#include "../src/core/vulkan/pipeline/ShaderModule.h"
+#include "../src/core/vulkan/pipeline/GraphicsPipeline.h"
+#include "../src/core/vulkan/descriptors/DescriptorPool.h"
+#include "../src/core/vulkan/descriptors/DescriptorSetLayout.h"
+#include "../src/core/vulkan/descriptors/DescriptorSet.h"
+#include "../src/core/3d/MeshManager.h"
 #include "../src/core/3d/Mesh.h"
+#include "../src/core/3d/Camera.h"
 
 #endif // BAAL_H
